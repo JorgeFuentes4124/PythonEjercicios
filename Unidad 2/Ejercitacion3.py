9e6cc7f8e99c4f10ca53221e75881626d1086c4b
daniel <danidex129@gmail.com>
2024-05-06 17:06:13 -0300

    Create Ejercitacion3.py

---
+++ Unidad 2/Ejercitacion3.py
@@ -0,0 +1,22 @@
+
+
+def Superficie(Lado1, lado2):
+    superficie = Lado1 * Lado2
+    return superficie
+
+Lado1 = int(input("Ingresa la longitud del primer lado del rectángulo: "))
+Lado2 = int(input("Ingresa la longitud del segundo lado del rectángulo: "))
+superficie = Superficie(Lado1, Lado2,)
+print("La superficie del rectángulo es:", superficie)
+
+
+
+
+
+
+
+
+
+
+
+
