70ca0ef891064828022bbfc3e2615bf84c94432f
daniel <danidex129@gmail.com>
2024-05-06 17:05:49 -0300

    Create Ejercicio 1.py

---
+++ Python Panchito/Ejercicios de python/Ejercicio 1.py
@@ -0,0 +1,12 @@
+
+
+meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Nomviembre", "Diciembre"]
+n=5
+for mes in range(len(meses)):
+   print(mes)
+
+if n == 5:
+   print(n)
+
+else:
+    print(n)
