b715fe8f0d87026bbe41e11647a49cf553e8abbf
daniel <danidex129@gmail.com>
2024-05-06 17:05:44 -0300

    Create Ejercitacion1.py

---
+++ Python Panchito/Unidad 2/Ejercitacion1.py
@@ -0,0 +1,14 @@
+
+
+def Suma():
+    n1 = int(input("Ingrese un número entero"))
+    n2 = int(input("Ingrese otro número entero"))
+
+    resultado = n1 + n2 
+
+    print("El resultado es:", resultado)
+
+Suma()
+
+
+
