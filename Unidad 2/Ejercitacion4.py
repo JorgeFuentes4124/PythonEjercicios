c240d685a0cb2ab174b08761a0fd327906c4be20
Jorge anashi <jorgee2007fc@gmail.com>
2024-05-06 17:29:35 -0300

    Update Ejercitacion4.py

--- Unidad 2/Ejercitacion4.py
+++ Unidad 2/Ejercitacion4.py
@@ -1,7 +1,14 @@
 
 
+def Superficie(Lado1,Lado2):
+    superficie = Lado1 * Lado2
+    return superficie
 
+Lado1 = int(input("Ingresa la longitud del lado del cuadrado:"))
+Lado2 = float(input("Ingresa la longitud del otro lado del cuadrado:"))
 
+superficie = Superficie(Lado1, Lado2)
+print("La superficie del Cuadrado es:", superficie)
 
 
 
