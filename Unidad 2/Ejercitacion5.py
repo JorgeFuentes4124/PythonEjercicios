0e0456062b6078aab74bcb5f842ef8c40714a415
Jorge anashi <jorgee2007fc@gmail.com>
2024-05-06 17:39:18 -0300

    Update Ejercitacion5.py

--- Unidad 2/Ejercitacion5.py
+++ Unidad 2/Ejercitacion5.py
@@ -1,5 +1,11 @@
 
 
+horas = int(input("Ingresa las Horas"))
+minutos = int(input("Ingresa los minutos"))
+segundos = int(input("Ingresa los segundos"))
+
+Segundos_Totales = horas * 3600 + minutos * 60 + segundos
+print("El Tiempo total es",Segundos_Totales)
 
 
 
