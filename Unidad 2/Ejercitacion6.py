4a2bf33360de4f244a02c3fde83d12c7c55d390d
afsafa <jorgee2007fc@gmail.com>
2024-05-13 14:29:43 -0300

    Update Ejercitacion6.py

--- Unidad 2/Ejercitacion6.py
+++ Unidad 2/Ejercitacion6.py
@@ -1,5 +1,10 @@
 
 
+base = float(input("Ingresa la base del triángulo: "))
+altura = float(input("Ingresa la altura del triángulo: "))
+superficie = 0.5 * base * altura
+
+print("La superficie del triángulo con base", base, "y altura", altura, "es:", superficie)
 
 
 
