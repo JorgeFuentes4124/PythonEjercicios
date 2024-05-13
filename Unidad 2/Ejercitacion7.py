63b5205a91c37f0f023916fee4c51e9a2e8d0989
afsafa <jorgee2007fc@gmail.com>
2024-05-13 16:53:20 -0300

    Update Ejercitacion7.py

--- Unidad 2/Ejercitacion7.py
+++ Unidad 2/Ejercitacion7.py
@@ -1,14 +1,16 @@
 
 
+def promedio_seis():
 
+    n1= float(input("Ingrese un número"))
+    n2= float(input("Ingrese otro número"))
+    n3= float(input("Ingrese otro número"))
+    n4= float(input("Ingrese otro número"))
+    n5= float(input("Ingrese otro número"))
+    n6= float(input("Ingrese otro número"))
 
+    suma = n1 + n2 + n3 + n4 + n5 + n6
+    promedio = 6 / suma
+    print("El promedio es",promedio)
 
-
-
-
-
-
-
-
-
-
+promedio_seis()
