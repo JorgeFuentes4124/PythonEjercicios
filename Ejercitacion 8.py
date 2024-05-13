841e1c3e7d85881c5e21b88fe3f42d7fa3ab6f08
afsafa <jorgee2007fc@gmail.com>
2024-05-13 14:49:20 -0300

    Update Ejercitacion 8.py

--- Ejercitacion 8.py
+++ Ejercitacion 8.py
@@ -1,7 +1,12 @@
 
+parcial = 50
+total = 200
 
+# Calcular el porcentaje como un número decimal
+porcentaje = (parcial / total) * 100
 
-
+# Imprimir el resultado
+print("El porcentaje que representa la cantidad parcial respecto a la cantidad total es:", porcentaje, "%")
 
 
 
