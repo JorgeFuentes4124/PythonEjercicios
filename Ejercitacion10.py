1e4abf2f483fa2d98fabbf5979fa6bc931fea9bf
afsafa <jorgee2007fc@gmail.com>
2024-05-13 15:16:12 -0300

    Create Ejercitacion10.py

---
+++ Ejercitacion10.py
@@ -0,0 +1,43 @@
+
+
+
+parciales = float(input("Ingrese la calificación de los exámenes parciales (0-10): "))
+practicos = float(input("Ingrese la calificación de los trabajos prácticos (0-10): "))
+integrador = float(input("Ingrese la calificación del examen integrador (0-10): "))
+
+# Calcular la contribución de cada aspecto a la calificación final
+contribucion_parciales = parciales * 0.3
+contribucion_practicos = practicos * 0.2
+contribucion_integrador = integrador * 0.5
+
+# Calcular la calificación final sumando las contribuciones de los tres aspectos
+calificacion_final = contribucion_parciales + contribucion_practicos + contribucion_integrador
+
+# Imprimir la calificación final
+print("La calificación final del estudiante es:", calificacion_final)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
