e2d61cf1fe2f480c7b9e1fd9fea8353af90bcfbb
afsafa <jorgee2007fc@gmail.com>
2024-05-13 16:19:25 -0300

    Update Ejercitacion 9.py

--- Ejercitacion 9.py
+++ Ejercitacion 9.py
@@ -3,12 +3,10 @@
 
 fecha = input("Ingrese la fecha en formato DDMMAAAA: ")
 
-# Extraer el día, mes y año utilizando descomposición matemática
 dia = int(fecha[:2])
 mes = int(fecha[2:4])
 año = int(fecha[4:])
 
-# Imprimir el día, mes y año por separado
 print("Día:", dia)
 print("Mes:", mes)
 print("Año:", año)
