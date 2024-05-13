75daa4ed1e699e3610becb82cb84bcec4ae80f05
afsafa <jorgee2007fc@gmail.com>
2024-05-13 16:42:16 -0300

    Create Ejercitacion 9.py

---
+++ Unidad 2/Ejercitacion 9.py
@@ -0,0 +1,28 @@
+
+
+
+fecha = input("Ingrese la fecha en formato DDMMAAAA: ")
+
+dia = int(fecha[:2])
+mes = int(fecha[2:4])
+año = int(fecha[4:])
+
+print("Día:", dia)
+print("Mes:", mes)
+print("Año:", año)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
