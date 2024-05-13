bed3336111190d0a3fbb2e4beb2b54156b71439d
afsafa <jorgee2007fc@gmail.com>
2024-05-13 16:19:27 -0300

    Update Ejercitacion10.py

--- Ejercitacion10.py
+++ Ejercitacion10.py
@@ -1,21 +1,16 @@
 
 
 
-parciales = float(input("Ingrese la calificación de los exámenes parciales (0-10): "))
-practicos = float(input("Ingrese la calificación de los trabajos prácticos (0-10): "))
-integrador = float(input("Ingrese la calificación del examen integrador (0-10): "))
+parciales = float(input("Ingrese la nota de los exámenes parciales (0-10): "))
+practicos = float(input("Ingrese la nota de los trabajos prácticos (0-10): "))
+integrador = float(input("Ingrese la nota del examen integrador (0-10): "))
 
-# Calcular la contribución de cada aspecto a la calificación final
 nota_parciales = parciales * 0.3
 nota_practicos = practicos * 0.2
 nota_integrador = integrador * 0.5
 
-# Calcular la calificación final sumando las contribuciones de los tres aspectos
-calificacion_final = nota_parciales + nota_practicos + nota_integrador
-
-# Imprimir la calificación final
-print("La calificación final del estudiante es:", calificacion_final)
-
+nota_final = nota_parciales + nota_practicos + nota_integrador
+print("La Nota final del estudiante es:", nota_final)
 
 
 
