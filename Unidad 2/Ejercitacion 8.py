bddd3e1ffd19cf2d4aa7b790793f9756904246bc
afsafa <jorgee2007fc@gmail.com>
2024-05-13 16:41:33 -0300

    Create Ejercitacion 8.py

---
+++ Unidad 2/Ejercitacion 8.py
@@ -0,0 +1,20 @@
+
+parcial = int(input("Ingresar la cantidad total")) #es mejor ingresar 50 aquí como valor recomendado
+total = int(input("Ingresar la cantidad total")) #es mejor ingresar 200 aquí como valor recomendado
+
+porcentaje = (parcial / total) * 100
+
+print("El porcentaje que representa la cantidad parcial respecto a la cantidad total es:", porcentaje, "%")
+
+
+
+
+
+
+
+
+
+
+
+
+
