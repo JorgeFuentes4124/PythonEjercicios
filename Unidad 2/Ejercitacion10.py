c8e328316c3972e66ac4b1af67aa14e7d837fe61
afsafa <jorgee2007fc@gmail.com>
2024-05-13 16:42:17 -0300

    Create Ejercitacion10.py

---
+++ Unidad 2/Ejercitacion10.py
@@ -0,0 +1,38 @@
+
+
+
+parciales = float(input("Ingrese la nota de los exámenes parciales (1-10): "))
+practicos = float(input("Ingrese la nota de los trabajos prácticos (1-10): "))
+integrador = float(input("Ingrese la nota del examen integrador (1-10): "))
+
+nota_parciales = parciales * 0.3
+nota_practicos = practicos * 0.2
+nota_integrador = integrador * 0.5
+
+nota_final = nota_parciales + nota_practicos + nota_integrador
+print("La Nota final del estudiante es:", nota_final)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
