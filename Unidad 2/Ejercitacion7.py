8c72f0fbd30fb8fa374fa1699f9d7522fae81f5b
afsafa <jorgee2007fc@gmail.com>
2024-05-13 16:19:29 -0300

    Update Ejercitacion7.py

--- Unidad 2/Ejercitacion7.py
+++ Unidad 2/Ejercitacion7.py
@@ -5,10 +5,9 @@ for i in range(6):
     numero = float(input("Ingrese el número real {}: ".format(i + 1)))
     numeros.append(numero)
 
-# Calcular el promedio
+
 promedio = sum(numeros) / len(numeros)
 
-# Mostrar el resultado
 print("El promedio de los números reales ingresados es:", promedio) 
 
 
