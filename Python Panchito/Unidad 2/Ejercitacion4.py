cb1c9d99f37e3ae32bd70988a7261981ac1d3907
JorgeFuentes4124 <jorgee2007fc@gmail.com>
2024-05-19 21:26:05 -0300

    Update Ejercitacion4.py

--- Python Panchito/Unidad 2/Ejercitacion4.py
+++ Python Panchito/Unidad 2/Ejercitacion4.py
@@ -1,5 +1,32 @@
 
 
+# Solicitar la fecha de nacimiento de la primera persona
+dia1 = int(input("Ingrese el día de nacimiento de la primera persona: "))
+mes1 = int(input("Ingrese el mes de nacimiento de la primera persona: "))
+año1 = int(input("Ingrese el año de nacimiento de la primera persona: "))
+
+# Solicitar la fecha de nacimiento de la segunda persona
+dia2 = int(input("Ingrese el día de nacimiento de la segunda persona: "))
+mes2 = int(input("Ingrese el mes de nacimiento de la segunda persona: "))
+año2 = int(input("Ingrese el año de nacimiento de la segunda persona: "))
+4
+# Comparar las fechas de nacimiento
+if año1 < año2:
+    print("La primera persona es mayor y se le asignará el primer lugar en la fila.")
+elif año1 > año2:
+    print("La segunda persona es mayor y se le asignará el primer lugar en la fila.")
+else:
+    if mes1 < mes2:
+        print("La primera persona es mayor y se le asignará el primer lugar en la fila.")
+    elif mes1 > mes2:
+        print("La segunda persona es mayor y se le asignará el primer lugar en la fila.")
+    else:
+        if dia1 < dia2:
+            print("La primera persona es mayor y se le asignará el primer lugar en la fila.")
+        elif dia1 > dia2:
+            print("La segunda persona es mayor y se le asignará el primer lugar en la fila.")
+        else:
+            print("Ambas personas tienen la misma fecha de nacimiento.")
 
 
 
